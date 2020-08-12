646a833652d8cbcdeb051de18e0db5f65d44e4ad
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-08-12 17:16:53 +0300

    remove augmentation

--- main.py
+++ main.py
@@ -90,20 +90,21 @@ def main():
 
 
 
-
-
-
-
-
     train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-                                        rotation_range=20,
-                                        zoom_range=0.15,
-                                        width_shift_range=0.2,
-                                        height_shift_range=0.2,
-                                        shear_range=0.15,
-                                        horizontal_flip=True,
-                                        fill_mode="nearest",
                                        validation_split=0.2)
+
+
+
+
+    # train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    #                                     rotation_range=20,
+    #                                     zoom_range=0.15,
+    #                                     width_shift_range=0.2,
+    #                                     height_shift_range=0.2,
+    #                                     shear_range=0.15,
+    #                                     horizontal_flip=True,
+    #                                     fill_mode="nearest",
+    #                                    validation_split=0.2)
 
     train_generator = train_datagen.flow_from_directory(args.ds_path,
                                                        subset="training",
